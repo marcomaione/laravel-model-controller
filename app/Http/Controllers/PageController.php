ad189869aca27086bc22a382384e6e0ec5851aee
marco maione <maionemarco14@gmail.com>
2022-03-30 17:11:34 +0200

    aggiornamento

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
+
+        $movies = Movie::all();
+        dd($movies);
         return view('home');
     }
 }
