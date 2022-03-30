ec6cfd1d3cf47bc044774e1d33e8715798ad946f
marco maione <maionemarco14@gmail.com>
2022-03-30 17:49:47 +0200

    esercizio finito

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,6 +10,7 @@ class PageController extends Controller
     public function index() {
 
         $movies = Movie::all();
-        return view('home', compact($movies));
+        return view('home', compact("movies"));
     }
+
 }
